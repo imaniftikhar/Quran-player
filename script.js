4db1f696ed1ca8f7351160592522b93bd48b8854
imaniftikhar <165419252+imaniftikhar@users.noreply.github.com>
2024-03-30 19:47:18 +0500

    Added Qari Selector and Customizer

--- script.js
+++ script.js
@@ -1,37 +1,75 @@
-const audio = document.getElementById('audio');
-const playBtn = document.getElementById('play');
-const pauseBtn = document.getElementById('pause');
-const stopBtn = document.getElementById('stop');
-const recitations = document.getElementById('recitations');
+// const audio = document.getElementById('audio');
+// const playBtn = document.getElementById('play');
+// const pauseBtn = document.getElementById('pause');
+// const stopBtn = document.getElementById('stop');
+// const recitations = document.getElementById('recitations');
 
-playBtn.addEventListener('click', () => {
-  audio.play();
-});
+// playBtn.addEventListener('click', () => {
+//   audio.play();
+// });
 
-pauseBtn.addEventListener('click', () => {
-  audio.pause();
-});
+// pauseBtn.addEventListener('click', () => {
+//   audio.pause();
+// });
 
-stopBtn.addEventListener('click', () => {
-  audio.pause();
-  audio.currentTime = 0;
-});
+// stopBtn.addEventListener('click', () => {
+//   audio.pause();
+//   audio.currentTime = 0;
+// });
 
-recitations.addEventListener('change', () => {
+// recitations.addEventListener('change', () => {
+//   audio.src = recitations.value;
+//   audio.play();
+// });
+// fetch('https://api.quran.sutanlab.id/surah')
+//   .then(response => response.json())
+//   .then(data => {
+//     data.data.forEach(surah => {
+//       surah.ayahs.forEach(ayah => {
+//         if (!qarisSelect.querySelector(`[value="${ayah.audio.primary}"]`)) {
+//           const option = document.createElement('option');
+//           option.value = ayah.audio.primary;
+//           option.textContent = ayah.audio.name;
+//           qarisSelect.appendChild(option);
+//         }
+//       });
+//     });
+//   });
+
+
+
+
+let audio = document.getElementById('audiosource');
+let audioplayer = document.getElementById('audio');
+let recitations = document.getElementById('recitations');
+let qaris = document.getElementById('qaris');
+let fontchanger = document.getElementById('fontchanger')
+let H1 = document.getElementById('H1')
+
+
+
+
+
+
+
+recitations.addEventListener('change', ()=>{
   audio.src = recitations.value;
-  audio.play();
+  audioplayer.load()
 });
-fetch('https://api.quran.sutanlab.id/surah')
-  .then(response => response.json())
-  .then(data => {
-    data.data.forEach(surah => {
-      surah.ayahs.forEach(ayah => {
-        if (!qarisSelect.querySelector(`[value="${ayah.audio.primary}"]`)) {
-          const option = document.createElement('option');
-          option.value = ayah.audio.primary;
-          option.textContent = ayah.audio.name;
-          qarisSelect.appendChild(option);
-        }
-      });
-    });
-  });
+
+qaris.addEventListener('change', ()=>{
+  let qariArray = recitations.value.split('/');
+  qariArray[4] = qaris.value;
+  let stringqariarray = qariArray.toString()
+  let fixedstringqariarray = stringqariarray.replaceAll(",", "/")
+  audio.src = fixedstringqariarray
+  audioplayer.load();
+});
+
+fontchanger.addEventListener('change', ()=>{
+  H1.style.fontFamily = fontchanger.value;
+});
+
+
+
+
